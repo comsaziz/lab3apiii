c4f5734047f06c869a7c3db3a354c2c944892a6c
comsaziz <comsaziz@gmail.com>
2024-09-16 14:57:36 +0300

    Add project

---
+++ main.js
@@ -0,0 +1,59 @@
+const apiUrl = "https://66e803d4b17821a9d9daf73c.mockapi.io/photo/photo";
+
+function postImage() {
+  let name = document.getElementById("inpName").value;
+  let url = document.getElementById("inpUrl").value;
+
+  if (!name || !url) {
+    alert("Please enter both name and URL.");
+    return;
+}
+
+  fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify({
+      name: name,
+      imageUrl: url,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Posted:", data);
+      getImages();
+    })
+    .catch((error) => console.error("Error posting data:", error));
+}
+
+function getImages() {
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      let cont = document.getElementById("contiuner");
+      cont.innerHTML = "";
+
+      data.forEach((element) => {
+        let itemDiv = document.createElement("div");
+
+        let img = document.createElement("img");
+        img.src = element.imageUrl;
+        img.alt = element.name;
+
+        let text = document.createElement("h1");
+        text.textContent = element.name;
+
+        itemDiv.appendChild(img);
+        itemDiv.appendChild(text);
+        cont.appendChild(itemDiv);
+      });
+    })
+    .catch((error) => console.error("Error fetching images:", error));
+}
+
+document.getElementById("btn").addEventListener("click", () => {
+  postImage();
+});
+
+getImages();
